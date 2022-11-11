ad1e9393070d4d7fa80d8096c312af058a72cde0
zhoushaofeng <zhoushaofeng@mail.ustc.edu.cn>
2022-11-11 10:51:48 +0800

    week3-part2

--- week3/linked_list/src/linked_list.rs
+++ week3/linked_list/src/linked_list.rs
@@ -1,60 +1,142 @@
 use std::fmt;
 use std::option::Option;
 
-pub struct LinkedList {
-    head: Option<Box<Node>>,
+struct Node<T> {
+    value: T,
+    next: Option<Box<Node<T>>>,
+}
+
+impl<T> Node<T> {
+    pub fn new(value: T, next: Option<Box<Node<T>>>) -> Node<T> {
+        Node {
+            value: value,
+            next: next,
+        }
+    }
+}
+
+impl<T: Clone> Clone for Node<T> {
+    fn clone(&self) -> Self {
+        Node {
+            value: self.value.clone(),
+            next: self.next.clone(),
+        }
+    }
+}
+
+impl<T: PartialEq> PartialEq for Node<T> {
+    fn eq(&self, other: &Node<T>) -> bool {
+        return self.value == other.value && self.next == other.next;
+    }
+}
+pub struct LinkedList<T> {
+    head: Option<Box<Node<T>>>,
     size: usize,
 }
 
-struct Node {
-    value: u32,
-    next: Option<Box<Node>>,
-}
-
-impl Node {
-    pub fn new(value: u32, next: Option<Box<Node>>) -> Node {
-        Node {value: value, next: next}
+/// 由于Option/Box/Node均实现Clone特征，LinkedList<T>的clone是从head开始沿list迭代clone的过程
+/// Option => Box => Node => Option ...
+impl<T: Clone> Clone for LinkedList<T> {
+    fn clone(&self) -> Self {
+        LinkedList {
+            head: self.head.clone(),
+            size: self.size,
+        }
     }
 }
 
-impl LinkedList {
-    pub fn new() -> LinkedList {
-        LinkedList {head: None, size: 0}
+impl<T: PartialEq> PartialEq for LinkedList<T> {
+    fn eq(&self, other: &LinkedList<T>) -> bool {
+        return self.size == other.size && self.head == other.head;
     }
-    
+}
+
+pub trait ComputeNorm {
+    fn compute_norm(&self) -> f64 {
+        0.0
+    }
+}
+
+impl ComputeNorm for LinkedList<f64> {
+    fn compute_norm(&self) -> f64 {
+        self.into_iter().map(|x| x * x).sum::<f64>().sqrt()
+    }
+}
+
+impl<T> Iterator for LinkedList<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.pop_front()
+    }
+}
+
+pub struct LinkedListIter<'a, T> {
+    current: &'a Option<Box<Node<T>>>,
+}
+
+impl<T: Clone> Iterator for LinkedListIter<'_, T> {
+    type Item = T;
+    fn next(&mut self) -> Option<T> {
+        match self.current {
+            Some(node) => {
+                self.current = &node.next;
+                Some(node.value.clone())
+            }
+            None => None,
+        }
+    }
+}
+
+impl<'a, T: Clone> IntoIterator for &'a LinkedList<T> {
+    type Item = T;
+    type IntoIter = LinkedListIter<'a, T>;
+    fn into_iter(self) -> Self::IntoIter {
+        Self::IntoIter {
+            current: &self.head,
+        }
+    }
+}
+
+impl<T> LinkedList<T> {
+    pub fn new() -> LinkedList<T> {
+        LinkedList {
+            head: None,
+            size: 0,
+        }
+    }
+
     pub fn get_size(&self) -> usize {
         self.size
     }
-    
+
     pub fn is_empty(&self) -> bool {
         self.get_size() == 0
     }
-    
-    pub fn push_front(&mut self, value: u32) {
-        let new_node: Box<Node> = Box::new(Node::new(value, self.head.take()));
+
+    pub fn push_front(&mut self, value: T) {
+        let new_node: Box<Node<T>> = Box::new(Node::new(value, self.head.take()));
         self.head = Some(new_node);
         self.size += 1;
     }
-    
-    pub fn pop_front(&mut self) -> Option<u32> {
-        let node: Box<Node> = self.head.take()?;
+
+    pub fn pop_front(&mut self) -> Option<T> {
+        let node: Box<Node<T>> = self.head.take()?;
         self.head = node.next;
         self.size -= 1;
         Some(node.value)
     }
 }
 
-
-impl fmt::Display for LinkedList {
+impl<T: fmt::Display> fmt::Display for LinkedList<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let mut current: &Option<Box<Node>> = &self.head;
+        let mut current: &Option<Box<Node<T>>> = &self.head;
         let mut result = String::new();
         loop {
             match current {
                 Some(node) => {
                     result = format!("{} {}", result, node.value);
                     current = &node.next;
-                },
+                }
                 None => break,
             }
         }
@@ -62,7 +144,7 @@ impl fmt::Display for LinkedList {
     }
 }
 
-impl Drop for LinkedList {
+impl<T> Drop for LinkedList<T> {
     fn drop(&mut self) {
         let mut current = self.head.take();
         while let Some(mut node) = current {
@@ -70,6 +152,3 @@ impl Drop for LinkedList {
         }
     }
 }
-
-
-
--- week3/linked_list/src/main.rs
+++ week3/linked_list/src/main.rs
@@ -1,22 +1,32 @@
-use linked_list::LinkedList;
 pub mod linked_list;
 
+use linked_list::{ComputeNorm, LinkedList};
+
 fn main() {
-    let mut list: LinkedList<u32> = LinkedList::new();
+    let mut list: LinkedList<f64> = LinkedList::new();
     assert!(list.is_empty());
     assert_eq!(list.get_size(), 0);
-    for i in 1..12 {
-        list.push_front(i);
+    let vec_str = vec![1.0, 2.0, 3.0];
+    for s in vec_str {
+        list.push_front(s.clone());
     }
-    println!("{}", list);
-    println!("list size: {}", list.get_size());
-    println!("top element: {}", list.pop_front().unwrap());
-    println!("{}", list);
-    println!("size: {}", list.get_size());
-    println!("{}", list.to_string()); // ToString impl for anything impl Display
+    println!("list = {}", list);
 
-    // If you implement iterator trait:
-    //for val in &list {
-    //    println!("{}", val);
-    //}
+    // test Clone
+    let list_2 = list.clone();
+
+    // test PartialEq
+    println!("list == list_2: {}", list == list_2);
+
+    // test ComputeNorm
+    println!("compute_norm(list) = {}", list.compute_norm());
+
+    // test Iterator and IntoIterator
+    for val in &list {
+        println!("{}", val);
+    }
+
+    for val in list {
+        println!("{}", val);
+    }
 }
